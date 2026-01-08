f2b981dc69dfbfc1bc571911e32cc819370d1dd4
transcental <amber@transcental.dev>
2026-01-08 10:38:17 +0100

    weh^2

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -3,6 +3,9 @@ from piccolo.engine.postgres import PostgresEngine
 
 from sharktank.config import config
 
-DB = PostgresEngine(config={"dsn": config.database_url.encoded_string()})
+DB = PostgresEngine(
+    config={"dsn": config.database_url.encoded_string()},
+    auto_create_extensions=False
+)
 
 APP_REGISTRY = AppRegistry(apps=["sharktank.piccolo_app"])
